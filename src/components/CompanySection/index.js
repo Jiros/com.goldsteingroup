48715a3dc3b1d177ec222b22575e3587341488b8
Jiros <jamie@munro.consulting>
2023-08-17 17:34:31 +0200

    Added companies

--- src/components/CompanySection/index.js
+++ src/components/CompanySection/index.js
@@ -20,7 +20,7 @@ function CompanySection() {
                     style={{ marginBottom: `var(--space-3)` }}
                 />
                 </div>
-                <p>With over 10 years of experience as an independent agency, the Allen Goldstein team boasts extensive local knowledge and truly personal service. Bespoke client interactions and customer satisfaction are at the heart of our process, allowing us to put you first in every deal.</p>
+                <p>Allen Goldstein was formed by a group of property specialists and offers a truly in-house comprehensive property consultancy service. From specific building details, yield, and contract terms to other technical letting and sales aspects of residential properties, Allen Goldstein has expertise in all areas. </p>
             </a>
         </div>
         <div class="card sh">
@@ -69,6 +69,70 @@ function CompanySection() {
                 />
                 </div>
                 <p>The Bil Sha Boxing Club focuses on boxing in its purest form, providing a friendly, high-energy environment with the latest equipment and modern facilities for boxers at all levels and ages. </p>
+            </a>
+        </div>
+        <div class="card up">
+            <a href="https://www.undisputedsports.promo/" target="_blank" rel="noreferrer">
+                <div>
+                <StaticImage
+                    src="../../images/undisputed.png"
+                    loading="eager"
+                    width={350}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt=""
+                    style={{ marginBottom: `var(--space-3)` }}
+                />
+                </div>
+                <p>Undisputed is a London-based boxing promotion created by Bilat Shaista, an experienced former boxer, entrepreneur, and owner of the Bil Sha Boxing Club. With a focus on safety, trust, and integrity, Undisputed strives to transform the current white collar boxing landscape and give boxers the chance to show off their talent with confidence.</p>
+            </a>
+        </div>
+        <div class="card cb">
+            <a href="https://cloudeberry.com/" target="_blank" rel="noreferrer">
+                <div>
+                <StaticImage
+                    src="../../images/cloudeberry.png"
+                    loading="eager"
+                    width={250}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt=""
+                    style={{ marginBottom: `var(--space-3)` }}
+                />
+                </div>
+                <p>CLOUDeBERRY is a responsive, client-focused Managed Service Provider (MSP) that provides end to end IT system support and maintenance. CLOUDeBERRY builds long term relationships and provides sustained support to businesses of any size.</p>
+            </a>
+        </div>
+        <div class="card vb">
+            <a href="https://vimbi.io" target="_blank" rel="noreferrer">
+                <div>
+                <StaticImage
+                    src="../../images/vimbi.png"
+                    loading="eager"
+                    width={200}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt=""
+                    style={{ marginBottom: `var(--space-3)` }}
+                />
+                </div>
+                <p>Vimbi is a Cloud and Technology Consultancy firm that offers high quality, cohesive solutions to clients through innovative strategic consultancy and software development. Vimbi's approach is to help organisation's evolve their current technology and processes.</p>
+            </a>
+        </div>
+        <div class="card gl">
+            <a href="https://glowlia.com/" target="_blank" rel="noreferrer">
+                <div>
+                <StaticImage
+                    src="../../images/glowlia.png"
+                    loading="eager"
+                    width={180}
+                    quality={95}
+                    formats={["auto", "webp", "avif"]}
+                    alt=""
+                    style={{ marginBottom: `var(--space-3)` }}
+                />
+                </div>
+                <p>Glowlia skincare products are for women who practise self-love and care and contain organic, evidence based ingredients. Glowlia offer a deliberately minimal selection of nourishing products that focus on anti-aging, pigmentation, hydration and achieving a glowing complexion.</p>
             </a>
         </div>
         <div class="card og">
